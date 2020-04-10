546b10172390de15f1c35d094df0afa42d558951
ym-first <541897923@qq.com>
2020-04-10 11:18:32 +0800

    Site updated: 2020-04-10 11:18:32

--- dist/music.js
+++ dist/music.js
@@ -1,19 +1,19 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
-    mini: true,
+    // mini: true,
     autoplay: false,
     audio: [
       {
         name: "My Way",
         artist: '张敬轩',
         url: 'http://music.163.com/song/media/outer/url?id=189243.mp3',
-        cover: '/blog/img/yuanbao.png',
+        cover: 'https://p1.music.126.net/7a77DZlspSaBZexOr8mdLw==/18863221486279082.jpg?param=40y40',
       },
       {
-        name: '听妈妈的话',
-        artist: '周杰伦',
-        url: 'http://www.ytmp3.cn/down/51577.mp3',
-        cover: '/blog/img/avatar.jpg',
+        name: '情人',
+        artist: 'Beyond',
+        url: 'http://music.163.com/song/media/outer/url?id=347241.mp3', 
+        cover: 'http://p2.music.126.net/pHr2hq_QGGDrM3JOedJyVQ==/93458488373022.jpg?param=40y40',
       }
     ]
 });
