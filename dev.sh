062cc60da2303ddebe1edc21451b60ca35776c16
ym-first <541897923@qq.com>
2021-03-01 17:48:03 +0800

    add autoshell

--- dev.sh
+++ dev.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+if [ $1 = d ];
+    then echo $1;
+    echo "hexo deploying...";
+    hexo clean && hexo d;
+    exit 1;
+fi
 echo `date`+"compressing js..."
 echo $1
 terser themes/tomotoes/source/js/main.js --output themes/tomotoes/source/js/main.min.js --compress --mangle
