42c6678e89775ea4678df2b08ba677233610f684
ym-first <541897923@qq.com>
2021-03-02 08:41:20 +0800

    add default content

--- dev.sh
+++ dev.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-if [ $1 = d ];
+
+if [ -n "$1" -a "$1" != " " ];
     then echo $1;
     echo "hexo deploying...";
     hexo clean && hexo d;
+    git add .
+    git commit -m "$1"
+    git push
     exit 1;
 fi
 echo `date`+"compressing js..."
