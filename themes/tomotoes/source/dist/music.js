2739d9a13a6304c35d2bc9e5b178c6d014163dd5
yangmin <yangmin@boswinner.com>
2024-08-24 18:59:54 +0800

    fix pics

--- themes/tomotoes/source/dist/music.js
+++ themes/tomotoes/source/dist/music.js
@@ -8,18 +8,6 @@ const ap = new APlayer({
             url: 'http://music.163.com/song/media/outer/url?id=94576.mp3',
             cover: 'http://p2.music.126.net/1rwehhpnhVJKrn-H33XAuA==/68169720922853.jpg?param=40y40',
         },
-        // {
-        //   name: '摘星',
-        //   artist: '陈百强',
-        //   url: 'http://music.163.com/song/media/outer/url?id=27874897.mp3', 
-        //   cover: 'http://p1.music.126.net/luoiKIagB63n7Y3bxfsZYQ==/109951163633483361.jpg?param=40y40',
-        // },
-        // {
-        //   name: '豆蔻战',
-        //   artist: '林姗姗',
-        //   url: 'http://music.163.com/song/media/outer/url?id=31365209.mp3', 
-        //   cover: 'http://p1.music.126.net/JeYPRRPZXYkWUjw5gUR3Eg==/109951164925550516.jpg?param=40y40',
-        // },
         {
             name: '遥望-Demo',
             artist: 'Beyond',
@@ -27,42 +15,17 @@ const ap = new APlayer({
             cover: 'http://p2.music.126.net/pHr2hq_QGGDrM3JOedJyVQ==/93458488373022.jpg?param=40y40',
         },
         // {
-        //   name: '不再犹豫-Live',
-        //   artist: 'Beyond',
-        //   url: 'http://music.163.com/song/media/outer/url?id=347583.mp3', 
-        //   cover: 'http://p2.music.126.net/pHr2hq_QGGDrM3JOedJyVQ==/93458488373022.jpg?param=40y40',
+        //     name: "不要怕-Live",
+        //     artist: '吉克隽逸',
+        //     url: 'http://music.163.com/song/media/outer/url?id=28936113.mp3',
+        //     cover: 'http://p2.music.126.net/0ZBYdn9Hm7_R-Z50pOwF6A==/8891750533913724.jpg?param=40y40',
         // },
         {
-            name: "不要怕-Live",
-            artist: '吉克隽逸',
-            url: 'http://music.163.com/song/media/outer/url?id=28936113.mp3',
-            cover: 'http://p2.music.126.net/0ZBYdn9Hm7_R-Z50pOwF6A==/8891750533913724.jpg?param=40y40',
+            name: "On Your Mask",
+            artist: 'CHAGE and ASKA',
+            url: 'http://music.163.com/song/media/outer/url?id=444676.mp3',
+            cover: 'https://p2.music.126.net/H4cVz3-qKtYDSHZZ-XFoYw==/734473767376838.jpg?param=40y40',
         },
-        // {
-        //   name: "Joe Le Taxi",
-        //   artist: '陈慧娴',
-        //   url: 'http://music.163.com/song/media/outer/url?id=212203.mp3',
-        //   cover: 'http://p1.music.126.net/3nOSHPz0MsQ42klQclDQlQ==/18850027346604601.jpg?param=40y40',
-        //   https://music.163.com/song?id=120845&userid=247206710
-        // },
-        // {
-        //   name: "心仍是冷",
-        //   artist: '梅艳芳/张学友',
-        //   url: 'http://music.163.com/song/media/outer/url?id=29809714.mp3',
-        //   cover: 'http://p1.music.126.net/277Ag7Ksu94nhJgXaj2A7Q==/109951163066639996.jpg?param=40y40',
-        // },
-        // {
-        //   name: "总有一天等到你",
-        //   artist: '张学友',
-        //   url: 'http://music.163.com/song/media/outer/url?id=190438.mp3',
-        //   cover: 'http://p2.music.126.net/G3WbwV7xfv-V-pVE36iTTA==/18255191556505451.jpg?param=40y40',
-        // },
-        // {
-        //   name: "ツナ覚醒-《家庭教师》插曲",
-        //   artist: '佐橋俊彦',
-        //   url: 'http://music.163.com/song/media/outer/url?id=29023858.mp3',
-        //   cover: 'http://p2.music.126.net/1KfEkhJ_C7mP9H2gav_wiQ==/109951163597262375.jpg?param=40y40',
-        // },
 
         {
             name: "分手总要在雨天-Live",
@@ -70,31 +33,17 @@ const ap = new APlayer({
             url: 'http://music.163.com/song/media/outer/url?id=187741.mp3',
             cover: 'http://p2.music.126.net/G3WbwV7xfv-V-pVE36iTTA==/18255191556505451.jpg?param=40y40',
         },
-        // {
-        //   name: "只有你不知道-Live",
-        //   artist: '张学友',
-        //   url: 'http://music.163.com/song/media/outer/url?id=189613.mp3',
-        //   cover: 'http://p2.music.126.net/G3WbwV7xfv-V-pVE36iTTA==/18255191556505451.jpg?param=40y40',
-        //   },
         {
-            name: "等",
-            artist: '陈百强',
-            url: 'http://music.163.com/song/media/outer/url?id=27874927.mp3',
-            cover: 'http://p1.music.126.net/luoiKIagB63n7Y3bxfsZYQ==/109951163633483361.jpg?param=40y40',
+            name: "じれったい",
+            artist: '玉置浩二',
+            url: 'http://music.163.com/song/media/outer/url?id=4910557.mp3',
+            cover: 'https://p2.music.126.net/gxL5_d0N-d_ECgpv17WTkQ==/109951164061543352.jpg?param=40y40',
         },
-
-        // {https://music.163.com/song?id=212203&userid=247206710
-        //   name: '情人',
-        //   artist: 'Beyond',
-        //   url: 'http://music.163.com/song/media/outer/url?id=347241.mp3', 
-        //   cover: 'http://p2.music.126.net/pHr2hq_QGGDrM3JOedJyVQ==/93458488373022.jpg?param=40y40',
-        // }
-
         // {
-        //   name: "My Way",
-        //   artist: '张敬轩',
-        //   url: 'http://music.163.com/song/media/outer/url?id=189243.mp3',
-        //   cover: 'https://p1.music.126.net/7a77DZlspSaBZexOr8mdLw==/18863221486279082.jpg?param=40y40',
+        //     name: "等",
+        //     artist: '陈百强',
+        //     url: 'http://music.163.com/song/media/outer/url?id=27874927.mp3',
+        //     cover: 'http://p1.music.126.net/luoiKIagB63n7Y3bxfsZYQ==/109951163633483361.jpg?param=40y40',
         // },
     ]
 });
